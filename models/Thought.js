bc751e18022947e3b6532e8ddfde963cac45112c
Dan B <dbraunby@hotmail.com>
2021-05-20 17:15:00 -0600

    models

---
+++ models/Thought.js
@@ -0,0 +1,57 @@
+const { Schema, model, Types } = require('mongoose');
+
+const reactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types,objectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: [280, 'Your reaction can only contain u to 280 characters.']
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+);
+
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min: [1, 'Your thought must contain at least one character.'],
+            max: [280, 'Your thought cannot contain more than 280 characters.']
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true
+        }, 
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
