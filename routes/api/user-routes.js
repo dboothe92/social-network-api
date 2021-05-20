46b1420c9759fba47ad6f87779f01fc566a008b2
Dan B <dbraunby@hotmail.com>
2021-05-20 17:31:57 -0600

    base routes set up

---
+++ routes/api/user-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+router
+    .route('/')  
+    .get()
+    .post()
+    
+router
+    .route('/:id')
+    .get()
+    .put()
+    .delete()
+
+router
+    .route('/:userId/friends/:friendId')
+    .post()
+    .delete()
+
+module.exports = router;
